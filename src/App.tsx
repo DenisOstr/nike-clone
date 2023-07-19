11b8360302c2fa6a7fce3e17cd56399d8f207940
Denis Ostrovsky <denisostrovskis1@gmail.com>
2023-07-19 22:00:26 +0300

    added product sorting by price

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,50 @@
 import { useState, useEffect } from 'react'
 
 import Header from './components/Header'
+import SortBy from './components/SortBy'
 import ProductCard from './components/ProductCard'
 
+import { Product, SortByList } from './types/Global'
+
 function App() {
-  const [products, setProducts] = useState([])
+  const sortByList: SortByList[] = [
+    { slug: 'low-high', title: 'Price: Low-High' },
+    { slug: 'high-low', title: 'Price: High-Low' }
+  ]
+
+  const [products, setProducts] = useState<Product[]>([])
+  const [sortBy, setSortBy] = useState<SortByList>(sortByList[0])
 
   const fetchProductData = () => {
     fetch('https://fakestoreapi.com/products').then(response => {
       return response.json()
     }).then(data => {
-      setProducts(data)
+      setProducts(
+        data.sort((a: Product, b: Product) => {
+          return a.price - b.price
+        })
+      )
     })
   }
 
   useEffect(() => {
     fetchProductData()
   }, [])
+
+  const handleProductSort = (sortBy: SortByList) => {
+    const productsWithSort = [...products]
+
+    productsWithSort.sort((a: Product, b: Product) => {
+      if (sortBy.slug == 'low-high') {
+        return a.price - b.price
+      } else {
+        return b.price - a.price
+      }
+    })
+
+    setSortBy(sortBy)
+    setProducts(productsWithSort)
+  }
 
   return (
     <>
@@ -25,23 +53,22 @@ function App() {
       <main className='main-container'>
         <header className='wall-header'>
           <h1 className='category-title'>Featured ({ products.length })</h1>
-
-          <div>
-            <p>Sort By: <span></span></p>
-          </div>
+          <SortBy sortByList={sortByList} currentSortBy={sortBy} callback={handleProductSort} />
         </header>
         
-        { 
-          products.length > 0 && (
-            <div className='product-grid'>
-              {
-                products.map((product: any) => (
-                  <ProductCard key={product.id} product={product} />
-                ))
-              }
-            </div>
-          ) 
-        }
+        <div>
+          { 
+            products.length > 0 && (
+              <div className='product-grid'>
+                {
+                  products.map((product: Product) => (
+                    <ProductCard key={product.id} product={product} />
+                  ))
+                }
+              </div>
+            ) 
+          }
+        </div>
       </main>
     </>
   )
